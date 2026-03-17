23347f02b9093850f951cb9bd2fa5fae4899de50
Mirel Volcan <mirelvolcan@MacBook-Pro.local>
2026-03-17 12:57:48 +0100

    ajuste: cambiar puerto del servidor a 5001

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ from src.webserver import app
 #ejecuta app, con debugger
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
